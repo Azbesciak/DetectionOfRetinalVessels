2729c2a5cfec94ce765a1959488cddc613fce5a2
Witold Kupś <witold.kups@interlan.pl>
2018-05-08 21:33:34 +0200

    Add NOT WORKING (JVM cpp crash) detector.

--- src/main/scala/com/witkups/retinalvessels/ImageProcessor.scala
+++ src/main/scala/com/witkups/retinalvessels/ImageProcessor.scala
@@ -16,8 +16,10 @@ import scala.language.postfixOps
 
 object ImageProcessor {
 
+
   val java2DFrameConverter = new Java2DFrameConverter()
   val converter = new OpenCVFrameConverter.ToIplImage
+
 
   implicit class MatUtils(val mat: Mat) {
     def toGray(): Mat = {
@@ -26,19 +28,26 @@ object ImageProcessor {
       res
     }
 
+    implicit class NumUtils(val num: Double) {
+      def between(a: Double, b: Double): Boolean = num <= b && num >= a
+    }
+
     val th = 30
+
     def canny(th1: Int = 0, th2: Int = 256, apertureSize: Int = 5): Mat = {
-      blur(mat, mat, (10,10))
+      blur(mat, mat, (10, 10))
       negate()
       equalizeHist(mat, mat)
       Canny(mat, mat, th1, th2, apertureSize, true /*L2 gradient*/)
       negate()
       contours()
-//      mat
     }
 
+    def filledMat(value: Int = 255)(implicit typ: Int = mat.`type`()): Mat = new Mat(mat.rows(), mat.cols(), typ, Scalar.all(value))
+
+
     def negate(): Unit = {
-      val filled = new Mat(mat.rows(), mat.cols(), mat.`type`(), Scalar.all(255))
+      val filled = filledMat()
       subtract(filled, mat, mat)
     }
 
@@ -70,19 +79,65 @@ object ImageProcessor {
       val image = java2DFrameConverter.convert(frame)
       toFXImage(image, null)
     }
+
+    implicit def toVector(): MatVector = new MatVector(mat)
+
+    def detect(): Mat = {
+      val (b, r, g) = splitRGB()
+      val clahe = createCLAHE(2.0, (8, 8))
+      clahe.apply(g, g)
+      val contrast_enhanced_green_fundus = g.clone()
+      val transformed = new Mat()
+      morphologyEx(g, transformed, MORPH_OPEN, getStructuringElement(MORPH_ELLIPSE, (5, 5)))
+      morphologyEx(transformed, transformed, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, (5, 5)))
+      morphologyEx(transformed, transformed, MORPH_OPEN, getStructuringElement(MORPH_ELLIPSE, (11, 11)))
+      morphologyEx(transformed, transformed, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, (11, 11)))
+      morphologyEx(transformed, transformed, MORPH_OPEN, getStructuringElement(MORPH_ELLIPSE, (23, 23)))
+      morphologyEx(transformed, transformed, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE, (23, 23)))
+      subtract(transformed, contrast_enhanced_green_fundus, transformed)
+      clahe.apply(transformed, transformed)
+      val threshed = new Mat()
+      threshold(transformed, threshed, 15, 255, THRESH_BINARY)
+      val cons = new MatVector()
+      findContours(threshed.clone(), cons, RETR_LIST, CHAIN_APPROX_SIMPLE)
+      val mask = filledMat()(0)
+      println(cons.size())
+      var counter = 0
+        (0L to cons.size()-1).toArray
+          .map(i => cons.get(i))
+          .withFilter(c => contourArea(c) <= 200)
+          .map(_.toVector())
+          .foreach { c =>
+            drawContours(mask, c, -1, 0)
+          }
+
+      bitwise_and(transformed, transformed, transformed, mask)
+      threshold(transformed, transformed, 15, 255, THRESH_BINARY_INV)
+      erode(transformed, transformed, getStructuringElement(MORPH_ELLIPSE, (3, 3)))
+      bitwise_not(transformed, transformed)
+      findContours(transformed.clone(), cons, RETR_LIST, CHAIN_APPROX_SIMPLE)
+      val xmask = filledMat()(0)
+      (0L until cons.size()).toParArray
+        .map(i => cons.get(i))
+        .foreach(cnt => {
+          val len = arcLength(cnt, true)
+          val aprox = new Mat()
+          approxPolyDP(cnt, aprox, 0.04 * len, false)
+          if (aprox.total() > 4 && contourArea(cnt).between(100, 3000)) {
+            drawContours(xmask, cnt.toVector(), -1, 0)
+          }
+        })
+      bitwise_and(transformed, transformed, xmask)
+      transformed
+    }
   }
+
 
   implicit def toSize(p: (Int, Int)): Size = new Size(p._1, p._2)
 
   implicit def toPoint(p: (Int, Int)): Point = new Point(p._1, p._2)
 
   implicit def toScalar(i: Int): Scalar = new Scalar(i)
-
-  implicit class IplImageUtils(val img: IplImage) {
-    def scaleToHeight(mat: Mat, height: Double): Unit = {
-
-    }
-  }
 
   implicit class ImgUtils(val img: Image) {
     def toMat(): Mat = {
@@ -98,9 +153,7 @@ class ImageProcessor(val original: Image, val map: Image, val fov: Image) {
 
   import ImageProcessor._
 
-  def process():Image = {
-    val grayMat = original toMat() splitRGB() _3
-
-    grayMat canny() toImage()
+  def process(): Image = {
+    original toMat() detect() toImage()
   }
 }
